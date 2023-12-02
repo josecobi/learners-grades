317330173355071b0ba8b6d96c1db9e37ffd952a
josecobi <58313777+josecobi@users.noreply.github.com>
2023-12-02 09:18:23 -0500

    initial commit

---
+++ jsfund.js
@@ -0,0 +1,14 @@
+// Understand data structures
+
+//DECLARE FUNCTIONS
+// Implement data validation logic(Check for errors in course Ids- try...catch)
+
+// Calculate weighted average
+
+// Calculate scores for each assignment(% and late submissions)
+
+// Store results(use an array?)
+
+// Create a function to get learners' data 
+
+//USE FUNCTIONS TO RETURN THE RESULTS
